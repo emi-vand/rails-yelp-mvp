932a4ee85bfff35dff008b3c5dfeb9bc7fafdb3f
Emilia Vandamme <emilia.audrey@gmail.com>
2020-08-12 16:18:10 +0100

    rails yelp mvp

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
-
 module.exports = environment
